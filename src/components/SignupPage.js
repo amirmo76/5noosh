16d34747b531937db1918aeeb8ad792fe62835ab
Amir Mohseni <amir.mohseni7697@yahoo.com>
2018-12-01 18:18:37 +0330

    added functions for calling api

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -3,54 +3,61 @@ import Navigation from './Navigation';
 import Footer from './Footer';
 
 
-const SignupPage = props => {
-    return (
-        <div className="signup">
-            <Navigation />
-            <div className="signup__body">
-                <div className="signup__hero"></div>
-                <div className="signup__box">
-                    <form className="signup__form">
-                        <div className="input__inline-group">
-                            <div className="input__group">
-                                <label className="label label--secondary">نام و نام خانوادگی</label>
-                                <input className="input input--secondary mg-bottom-md" type="text" required/>
-                            </div>
+export default class SignupPage extends React.Component {
 
-                            <div className="input__group">
-                                <label className="label label--secondary">ایمیل</label>
-                                <input className="input input--secondary mg-bottom-md" type="email" required/>
-                            </div>
-                        </div>
-                        
-                        <label className="label label--secondary">شماره همراه</label>
-                        <input className="input input--secondary mg-bottom-md" type="tel" required/>
-                        <label className="label label--secondary">آدرس</label>
-                        <input className="input input--secondary mg-bottom-md" type="text" required/>
+    submitHandler = e => {
+        e.preventDefault();
+        //-------------CODE GOES HERE----------
+        
+    }
 
-                        <div className="input__inline-group">
-                            <div className="input__group">
-                                <label className="label label--secondary">رمز عبور</label>
-                                <input className="input input--secondary mg-bottom-md" type="password" required/>
+    render(){
+        return (
+            <div className="signup">
+                <Navigation />
+                <div className="signup__body">
+                    <div className="signup__hero"></div>
+                    <div className="signup__box">
+                        <form className="signup__form">
+                            <div className="input__inline-group">
+                                <div className="input__group">
+                                    <label className="label label--secondary">نام و نام خانوادگی</label>
+                                    <input className="input input--secondary mg-bottom-md" type="text" required/>
+                                </div>
+    
+                                <div className="input__group">
+                                    <label className="label label--secondary">ایمیل</label>
+                                    <input className="input input--secondary mg-bottom-md" type="email" required/>
+                                </div>
                             </div>
-
-                            <div className="input__group">
-                                <label className="label label--secondary">تایید رمز عبور</label>
-                                <input className="input input--secondary mg-bottom-lg" type="password" required/>
+                            
+                            <label className="label label--secondary">شماره همراه</label>
+                            <input className="input input--secondary mg-bottom-md" type="tel" required/>
+                            <label className="label label--secondary">آدرس</label>
+                            <input className="input input--secondary mg-bottom-md" type="text" required/>
+    
+                            <div className="input__inline-group">
+                                <div className="input__group">
+                                    <label className="label label--secondary">رمز عبور</label>
+                                    <input className="input input--secondary mg-bottom-md" type="password" required/>
+                                </div>
+    
+                                <div className="input__group">
+                                    <label className="label label--secondary">تایید رمز عبور</label>
+                                    <input className="input input--secondary mg-bottom-lg" type="password" required/>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className="signup__options">
-                            <a className="mg-left-lg">!قبلا ثبت نام کرده اید؟ برای ورود کلیک کنید</a>
-                            <button className="btn btn--outline btn--fat btn--tall btn--primary" type="submit">ثبت نام</button>
-                        </div>
-                    </form>
+    
+                            <div className="signup__options">
+                                <a className="mg-left-lg">!قبلا ثبت نام کرده اید؟ برای ورود کلیک کنید</a>
+                                <button className="btn btn--outline btn--fat btn--tall btn--primary" type="submit" onClick={this.submitHandler}>ثبت نام</button>
+                            </div>
+                        </form>
+                    </div>
+                    
                 </div>
-                
+                <Footer />
             </div>
-            <Footer />
-        </div>
-    );
+        );
+    } 
 }
-
-export default SignupPage;
