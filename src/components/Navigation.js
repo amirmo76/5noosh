346fecdd2958176f639c2778b8fc0b991bbabfd8
amirmo76 <amir.mohseni7697@yahoo.com>
2019-02-13 18:32:42 +0330

    nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -133,6 +133,7 @@ export default class Navigation extends React.Component {
             }
         }).then(function (response) {
             if (response.status === 200) {
+                console.log(response.data.data.profile_pic);
                 const pic = response.data.data.profile_pic;
                 bind.setState(() => ({
                     avatar: pic,
